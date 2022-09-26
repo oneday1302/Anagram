f0f92f9880f5123bc8438c2b162e01bdd9da82c5
oneday1302 <kovalchukvladyslav2gmail.com>
2022-09-26 18:28:47 +0200

    add method reverse

--- Anagram/src/ua/foxmainded/javaspring/anagram/Anagram.java
+++ Anagram/src/ua/foxmainded/javaspring/anagram/Anagram.java
@@ -4,6 +4,23 @@ public class Anagram
 {
 	public static void main(String[] args)
 	{
+		System.out.println(reverse("Hello,"));
+	}
+
+	public static String reverse(String word)
+	{
+		String reverseWord = new StringBuilder(word.replaceAll("[^A-Za-z]+", "")).reverse().toString();
+		StringBuffer reverseWordBuffer = new StringBuffer(reverseWord);
+		char[] letters = word.toCharArray();
 		
+		for (int i = 0; i < letters.length; i++)
+		{
+			if (!Character.isLetter(letters[i]))
+			{
+				reverseWordBuffer.insert(i, letters[i]);
+			}
+		}
+		reverseWord = reverseWordBuffer.toString();
+		return reverseWord;
 	}
 }
